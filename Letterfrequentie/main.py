55aec118d3e905e8f00481abd84497d36ad9d731
UnknownZandbak <rjwillar01@gmail.com>
2022-05-25 16:20:02 +0200

    BIg Change to structure of books and made a notebook for main python file

--- Letterfrequentie/main.py
+++ Letterfrequentie/main.py
@@ -1,8 +1,7 @@
-from optparse import Values
 import numpy as np
 # import the texts as strings
-from data.booknl import textNL
-from data.booken import textEN
+from data.booknl import bookNL
+from data.booken import bookEN
 
 index_lookup = {
     "a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, "i": 8, "j": 9,
@@ -25,14 +24,14 @@ def pre_procces(text: str) -> str:
     
     return text
 
-def frequency(text: str) -> dict:
+def getFreqMatrix(text: str) -> np.array:
     freq = {}
 
     for i in range(len(text)-1):
         bigramm = f"{text[i]}{text[i+1]}"
         freq[f"{bigramm}"] = freq[f"{bigramm}"]+1 if bigramm in freq else 1
-
-    return freq
+        matrix = to_freq_matrix(freq)
+    return matrix
 
 def to_freq_matrix(freqdict: dict) -> np.array:
     freqmatrix = np.zeros((28, 28), dtype=int)
@@ -41,13 +40,37 @@ def to_freq_matrix(freqdict: dict) -> np.array:
         freqmatrix[index_lookup[char1 if char1 in index_lookup else "&"]][index_lookup[char2 if char2 in index_lookup else "&"]] = freqdict[bigramm]
     return freqmatrix
 
+def predict_language(sentence: str):
+    """ prediction model of with language the given sentence is writen in.
+    Predicts between English and Dutch. 
+
+    Args:
+        sentence (str): Given sentence to predict the language of.
+    """
+    # generate matrix for sentence
+    matrix_sentence = getFreqMatrix(sentence)
+
+    diff_score_nl = sum(matrix_bookNL * matrix_sentence)
+    diff_score_en = sum(matrix_bookEN * matrix_sentence)
+
+    print(diff_score_en, diff_score_nl)
+
+
 if __name__ == "__main__" :
-    textEN = pre_procces(textEN)
-    freqEN = frequency(textEN)
-    freq_matrixEN = to_freq_matrix(freqEN)
+    # procces the english book and generate a freq matrix for it
+    bookEN = pre_procces(bookEN)
+    print(f"Pre-proccesing done on english book")
+    print("generating Matrix")
+    matrix_bookEN = getFreqMatrix(bookEN)
+    print("Done")
 
-    # textNL = pre_procces(textNL)
-    # freqNL = frequency(textNL)
-    # freq_matrixNL = to_freq_matrix(freqNL)
+    # procces the dutch book and generate a freq matrix for it
+    bookNL = pre_procces(bookNL)
+    print(f"Pre-proccesing done on dutch book")
+    print("generating Matrix")
+    matrix_bookNL = getFreqMatrix(bookNL)
+    print("Done")
 
-    print(freq_matrixEN)
+    predict_language("""And so it was indeed! She was now only ten inches high, and her face
+                        brightened up at the thought that she was now the right size for going
+                        through the little door into that lovely garden.""")
